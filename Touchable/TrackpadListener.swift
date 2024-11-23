601c46a078ddf358d3c1cac16cb49b8fd66391fe
leostudiooo <lilingfeng0408@outlook.com>
2024-11-24 01:48:07 +0800

    Visualized X, Y movement; optimize layout

--- Touchable/TrackpadListener.swift
+++ Touchable/TrackpadListener.swift
@@ -6,7 +6,6 @@
 //
 
 import AppKit
-import CoreGraphics
 
 struct TrackpadDataPoint: Identifiable {
     let id: Int
@@ -34,7 +33,7 @@ class TrackpadListener: ObservableObject {
     }
     
     private func startTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { [weak self] _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { [weak self] _ in
             guard let self = self else { return }
             DispatchQueue.main.async {
                 if self.trackpadPressure > 0 {
